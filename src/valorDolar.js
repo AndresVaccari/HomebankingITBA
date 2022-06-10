de3baf2e461d9f02ea2d1a14a1c3652c4916f2fb
AndresVaccari <andresvaccari34@gmail.com>
2022-06-10 12:41:30 -0300

    Agregadas imagenes dolar

--- src/valorDolar.js
+++ src/valorDolar.js
@@ -3,18 +3,37 @@ const xmlHttp = new XMLHttpRequest();
 const HTMLResponse = document.getElementById("dolar");
 const ul = document.createElement("ul");
 const tiposDolar = ["Dolar Oficial", "Dolar Blue", "Dolar turista"];
+const dolarSoloVenta = ["Dolar turista"];
+const imagenesDolar = [
+  "img/dolar/oficial.png",
+  "img/dolar/blue.png",
+  "img/dolar/turista.png",
+];
 
 (async function getData() {
   const response = await fetch(API_URL);
   const data = await response.json();
+  let cont = 0;
   data.forEach((tipo) => {
     if (tiposDolar.includes(tipo.casa.nombre)) {
       const li = document.createElement("li");
       const p = document.createElement("p");
-      const texto = document.createTextNode(
-        `${tipo.casa.nombre} - Compra: ${tipo.casa.compra} - Venta: ${tipo.casa.venta}`
-      );
+      const img = document.createElement("img");
+      let texto;
+      img.src = imagenesDolar[cont];
+      if (dolarSoloVenta.includes(tipo.casa.nombre)) {
+        texto = document.createTextNode(
+          `${tipo.casa.nombre} - Venta: ${tipo.casa.venta}  `
+        );
+      } else {
+        texto = document.createTextNode(
+          `${tipo.casa.nombre} - Compra: ${tipo.casa.compra} - Venta: ${tipo.casa.venta}  `
+        );
+      }
+
       p.appendChild(texto);
+      p.appendChild(img);
+      cont++;
       li.appendChild(p);
       li.className = "list-group-item";
       ul.appendChild(li);
