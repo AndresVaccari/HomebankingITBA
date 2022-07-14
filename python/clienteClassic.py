20c92bb6123148e2e42fd4c37e9c9f58547b9a2b
Andres Vaccari <andresvaccari34@gmail.com>
2022-07-14 10:37:41 -0300

    Fix

--- python/clienteClassic.py
+++ python/clienteClassic.py
@@ -4,8 +4,8 @@ from cliente import Cliente
 class ClienteClassic(Cliente):
     def __init__(self, nombre, apellido, dni, direccion, telefono, email):
         maximoNegativo = 0
-        cantMaxTarjetas = 0
-        cantMaxChequeras = 0
+        cantMaxTarjetas = 1
+        cantMaxChequeras = 1
         comisionTransferencia = 0.01
         super().__init__(
             nombre,
