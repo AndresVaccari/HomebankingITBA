f4940f4d8bfbecab0f9bd70ff578cacb3dce9804
Andres Vaccari <andresvaccari34@gmail.com>
2022-07-16 12:39:34 -0300

    fix

--- python/moduloCliente/razonRetiroEfectivo.py
+++ python/moduloCliente/razonRetiroEfectivo.py
@@ -1,7 +1,6 @@
 class RazonRetiroEfectivo:
     def resolver(self, cliente, cantidadARetirar, MontoMaximo):
-        if cliente.cantidadARetirar > MontoMaximo():
+        if cliente.cantidadARetirar < MontoMaximo:
             return "Puede retirar efectivo"
         else:
             return "Cantidad maxima de retiro de efectivo alcanzada"
-
--- python/moduloCliente/razonTransferenciaEnviada.py
+++ python/moduloCliente/razonTransferenciaEnviada.py
@@ -1,6 +1,6 @@
 class RazonTransferenciaEnviada:
     def resolver(self, cliente, SaldoEnCuenta, MontoEnviado):
-        if cliente.SaldoEnCuenta > MontoEnviado():
+        if cliente.SaldoEnCuenta > MontoEnviado:
             return "Puede realizar la transferencia"
         else:
             return "No puede realizar la transferencia, el monto enviado supera el saldo"
