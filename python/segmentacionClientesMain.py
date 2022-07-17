060231d033225748f89911d9ffce25d248371c0b
Andres Vaccari <andresvaccari34@gmail.com>
2022-07-17 19:22:34 -0300

    Comentario

--- python/segmentacionClientesMain.py
+++ python/segmentacionClientesMain.py
@@ -2,6 +2,7 @@ import os
 
 from moduloSegmentacionCliente.segmentacionClientes import main
 
+# Definicion de los directorios de los archivos de json
 ARCHIVOS = (
     os.path.join(os.path.dirname(os.path.realpath(__file__)), "ejemplosJson\eventosBlack.json"),
     os.path.join(os.path.dirname(os.path.realpath(__file__)), "ejemplosJson\eventosClassic.json"),
