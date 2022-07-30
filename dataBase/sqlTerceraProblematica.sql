5043b972f36bd9019186ff4c5db0c6f6a1311cb5
catemlekuz <cate2012.ml@gmail.com>
2022-07-30 12:23:47 -0300

    arreglo

--- dataBase/sqlTerceraProblematica.sql
+++ dataBase/sqlTerceraProblematica.sql
@@ -11,6 +11,8 @@ WHERE Apellido LIKE '%z%'
 -- Seleccionar el nombre, apellido, edad y nombre de sucursal de las personas con nombre “Brendan” y ordenar el resultado por nombre de sucursal
 SELECT Nombre, Apellido, Edad, sucursal.branch_name as 'Nombre Sucursal'
 FROM vCliente
+WHERE Nombre LIKE 'Brendan' 
+ORDER BY sucursal
 INNER JOIN sucursal ON SucursalID = sucursal.branch_id
 
 -- Seleccionar de tabla de préstamos los préstamos con un importe mayor a $80.000 y los préstamos prendarios
