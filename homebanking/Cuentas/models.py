d1665f79fba1de8e618bf7eedff60549cfa10dd9
Andres Vaccari <andresvaccari34@gmail.com>
2022-08-10 16:30:04 -0300

    Fix

--- homebanking/Cuentas/models.py
+++ homebanking/Cuentas/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Cuenta(models.Model):
     account_id = models.AutoField(primary_key=True)
-    customer_id = models.CharField(max_length=255)
-    balance = models.CharField(max_length=None)
+    customer_id = models.ForeignKey("Cliente", on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=30, decimal_places=2)
     iban = models.CharField(max_length=255)
     limiteExtraccionDiario = models.DecimalField(max_digits=5, decimal_places=2)
     limiteTransferenciaRecibida = models.DecimalField(max_digits=5, decimal_places=2)
     costorTransferenciaRecibida = models.DecimalField(max_digits=5, decimal_places=2)
     saldoDescubiertoDisponible = models.DecimalField(max_digits=5, decimal_places=2)
-    tipoCuenta = models.CharField(max_length=None)
+    tipoCuenta = models.CharField(max_length=255)
