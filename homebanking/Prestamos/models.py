33690d635f925fd2d5a8c4493b676794fec4e8dd
Andres Vaccari <andresvaccari34@gmail.com>
2022-08-10 16:27:16 -0300

    Fix

--- homebanking/Prestamos/models.py
+++ homebanking/Prestamos/models.py
@@ -5,5 +5,5 @@ class Prestamos(models.Model):
     loan_id = models.AutoField(primary_key=True)
     loan_type = models.CharField(max_length=11)
     loan_date = models.CharField(max_length=10)
-    loan_total = models.CharField(max_length= 9)
+    loan_total = models.DecimalField(max_digits=30, decimal_places=2)
     customer_id = models.ForeignKey("Cliente", on_delete=models.CASCADE)
