a22466cdcea0cf66663b18701349da49124ccf17
carladifilippo <cdifilippo@frba.utn.edu.ar>
2022-08-10 16:26:01 -0300

    prestamos

--- homebanking/Prestamos/models.py
+++ homebanking/Prestamos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Prestamos(models.Model):
+    loan_id = models.AutoField(primary_key=True)
+    loan_type = models.CharField(max_length=11)
+    loan_date = models.CharField(max_length=10)
+    loan_total = models.CharField(max_length= 9)
+    customer_id = models.ForeignKey("Cliente", on_delete=models.CASCADE)
