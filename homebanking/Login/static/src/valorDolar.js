087717a21e52702c7a0321426c3790c73bde1c56
Andres Vaccari <andresvaccari34@gmail.com>
2022-08-13 18:04:05 -0300

    Correccion imagenes dolar

--- homebanking/Login/static/src/valorDolar.js
+++ homebanking/Login/static/src/valorDolar.js
@@ -4,9 +4,9 @@ const ul = document.createElement("ul");
 const tiposDolar = ["Dolar Oficial", "Dolar Blue", "Dolar turista"];
 const dolarSoloVenta = ["Dolar turista"];
 const imagenesDolar = [
-  "img/dolar/oficial.png",
-  "img/dolar/blue.png",
-  "img/dolar/turista.png",
+  "https://i.imgur.com/MmZJyKJ.png",
+  "https://i.imgur.com/znv3O8M.png",
+  "https://i.imgur.com/jG5ui3i.png",
 ];
 
 (async function getData() {
@@ -22,7 +22,7 @@ const imagenesDolar = [
       img.src = imagenesDolar[cont];
       img.height = "20";
       if (dolarSoloVenta.includes(tipo.casa.nombre)) {
-        p.innerHTML = `${tipo.casa.nombre} - Venta: <span class="valorDolar">${tipo.casa.venta}</span> `;
+        p.innerHTML = `${tipo.casa.nombre} - Venta: <span class="valorDolar">${tipo.casa.venta}</span>`;
       } else {
         texto = document.createTextNode(`${tipo.casa.nombre} `);
         p.innerHTML = `${tipo.casa.nombre} - Compra: <span class="valorDolar">${tipo.casa.compra}</span> - Venta: <span class="valorDolar">${tipo.casa.venta}</span> `;
