ee84ff565db14cd2ff1e6be8490b4268f230d35f
Andres Vaccari <andresvaccari34@gmail.com>
2022-08-13 16:41:59 -0300

    Agregado usuario a cliente

--- homebanking/Clientes/models.py
+++ homebanking/Clientes/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class AuditoriaCuenta(models.Model):
@@ -93,6 +94,7 @@ class AuthUserUserPermissions(models.Model):
 
 
 class Cliente(models.Model):
+    usuario = models.OneToOneField(User, models.DO_NOTHING, db_column="usuario", null=True, blank=True)
     customer_id = models.AutoField(primary_key=True)
     customer_name = models.TextField()
     customer_surname = models.TextField()  # This field type is a guess.
@@ -104,7 +106,6 @@ class Cliente(models.Model):
     )  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = "cliente"
 
 
