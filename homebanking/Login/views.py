ff999337eacb056106b305ab45f9853278101a87
Andres Vaccari <andresvaccari34@gmail.com>
2022-08-13 21:24:57 -0300

    Cambios

--- homebanking/Login/views.py
+++ homebanking/Login/views.py
@@ -11,13 +11,5 @@ def login(request):
     return render(request, "Login/login.html")
 
 
-def homebanking(request):
-    return render(request, "homebanking.html")
-
-
-def gastos(request):
-    return render(request, "gastos.html")
-
-
 # def register(request):
 #     return render(request, "register.html")
